8af3fff12b9bb78e05efb565ce3a5347bba2ac11
okawak <kodak2022@outlook.jp>
2025-05-17 18:24:21 +0900

    feat: wasm function

--- parse_message/src/lib.rs
+++ parse_message/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn to_md(author: &str, content: &str, iso_ts: &str) -> String {
+    format!("- **{}** ({})\n{}\n\n", author, iso_ts, content)
 }
