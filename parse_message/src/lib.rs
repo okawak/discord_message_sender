e02f7a10385fc267fa4ba42a4476985509654211
okawak <kodak2022@outlook.jp>
2025-05-24 17:17:40 +0900

    feat: dummy wasm function

--- parse_message/src/lib.rs
+++ parse_message/src/lib.rs
@@ -1,6 +1,23 @@
+use serde::Serialize;
+use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn to_md(author: &str, content: &str, iso_ts: &str) -> String {
-    format!("- **{}** ({})\n{}\n\n", author, iso_ts, content)
+#[derive(Serialize)]
+pub struct ParseMessageResult<'a> {
+    pub md: &'a str,
+    pub is_clip: bool,
+    pub name: &'a str, // no need .md extension
 }
+
+#[wasm_bindgen]
+pub fn process_message(input: &str, prefix: &str) -> JsValue {
+    let dummy = ParseMessageResult {
+        md: "> **Test Markdown**\n\n— *Dummy Bot*",
+        is_clip: false,
+        name: input,
+    };
+    to_value(&dummy).unwrap()
+}
+
+#[wasm_bindgen(start)]
+pub fn init() {}
