41193c84d681bf9d824f7b41c1a4d0027dca9253
okawak <kodak2022@outlook.jp>
2026-09-11 19:52:46 +0900

    Escape forbidden YAML noncharacters

--- crates/html_to_markdown/src/frontmatters.rs
+++ crates/html_to_markdown/src/frontmatters.rs
@@ -20,7 +20,12 @@ pub(crate) fn serialize_yaml_string(value: &str) -> String {
         match character {
             '"' => serialized.push_str("\\\""),
             '\\' => serialized.push_str("\\\\"),
-            '\u{0000}'..='\u{001f}' | '\u{007f}'..='\u{009f}' | '\u{2028}' | '\u{2029}' => {
+            '\u{0000}'..='\u{001f}'
+            | '\u{007f}'..='\u{009f}'
+            | '\u{2028}'
+            | '\u{2029}'
+            | '\u{fffe}'
+            | '\u{ffff}' => {
                 use std::fmt::Write;
                 write!(serialized, "\\u{:04X}", character as u32)
                     .expect("writing to a String cannot fail");
@@ -70,8 +75,8 @@ mod tests {
     #[case("123", r#""123""#)]
     #[case(r#""quoted" \ path"#, r#""\"quoted\" \\ path""#)]
     #[case(
-        "line\ncarriage\rtab\t\0\u{001f}\u{007f}\u{0085}\u{2028}\u{2029}",
-        r#""line\u000Acarriage\u000Dtab\u0009\u0000\u001F\u007F\u0085\u2028\u2029""#
+        "line\ncarriage\rtab\t\0\u{001f}\u{007f}\u{0085}\u{2028}\u{2029}\u{fffe}\u{ffff}",
+        r#""line\u000Acarriage\u000Dtab\u0009\u0000\u001F\u007F\u0085\u2028\u2029\uFFFE\uFFFF""#
     )]
     fn serializes_as_a_single_yaml_string(#[case] value: &str, #[case] expected: &str) {
         assert_eq!(serialize_yaml_string(value), expected);
